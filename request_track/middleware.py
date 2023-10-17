8acabd64c2e2b2e86bff9a1bf3179faa17c44201
PooyaRezaee <pooya.rezaee.official@gmail.com>
2023-10-17 14:14:40 +0330

    Convert function get_object_ip to Two function 'get_or_create_object' and 'get_ip_address'

--- request_track/middleware.py
+++ request_track/middleware.py
@@ -1,5 +1,5 @@
 from .models import RequestLog
-from .utils import get_object_ip
+from .utils import get_or_create_object_ip,get_ip_address
 
 class LoggingRequestMiddleware:
     def __init__(self, get_response):
@@ -8,13 +8,15 @@ class LoggingRequestMiddleware:
     def __call__(self, request):
         response = self.get_response(request)
 
+        ip_address = get_ip_address(request)
+
         RequestLog.objects.create(
-            ip=get_object_ip(request),
+            ip=get_or_create_object_ip(ip_address),
             user=request.user if request.user.is_authenticated else None,
             method=request.method,
             route=request.path,
             status_code=response.status_code,
-            user_agent=request.META.get('HTTP_USER_AGENT', ''),
+            user_agent=request.META.get("HTTP_USER_AGENT", ""),
             query_params=request.GET,
         )
 
--- request_track/utils.py
+++ request_track/utils.py
@@ -1,12 +1,16 @@
 from .models import IpAddress
 
-def get_object_ip(request):
-    x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+def get_ip_address(request):
+    x_forwarded_for = request.META.get("HTTP_X_FORWARDED_FOR")
+    
     if x_forwarded_for:
-        ip = x_forwarded_for.split(',')[0]
+        ip = x_forwarded_for.split(",")[0]
     else:
-        ip = request.META.get('REMOTE_ADDR')
+        ip = request.META.get("REMOTE_ADDR")
 
+    return ip
+
+def get_or_create_object_ip(ip):
     if IpAddress.objects.filter(ip=ip).exists():
         return IpAddress.objects.get(ip=ip)
     else:
